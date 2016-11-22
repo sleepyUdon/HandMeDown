b913a5bd1d329913dfbf72b9ade9f90812d2678d
Viviane Chan <vivianechan@hotmail.com>
2016-11-22 16:51:52 -0500

    Initial Commit

---
+++ HandMeDown/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HandMeDown
+//
+//  Created by Viviane Chan on 2016-11-22.
+//  Copyright © 2016 Viviane Chan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
