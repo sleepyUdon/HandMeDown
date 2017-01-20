a9959a27828fa1fcf56854e8989455d82440d8d5
Viviane Chan <vivianechan@Vivianes-MacBook-Pro.local>
2017-01-20 11:19:16 -0500

    Rever to a54ce8ebfda1019d7f691dcdfc8ac72c13c9262a

--- DerivedData/HandMeDown/Build/Intermediates/Pods.build/Debug-iphoneos/Pods-HandMeDown.build/Objects-normal/arm64/Pods-HandMeDown-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/vivianechan/Documents/1.0\ Projects/2.0\ HandMeDown/HandMeDown/Pods/Target\ Support\ Files/Pods-HandMeDown/Pods-HandMeDown-dummy.m \
-  /Users/vivianechan/Documents/1.0\ Projects/2.0\ HandMeDown/HandMeDown/Pods/Firebase/Core/Sources/module.modulemap
--- DerivedData/HandMeDown/Build/Intermediates/Pods.build/Debug-iphoneos/Realm.build/Objects-normal/arm64/Realm-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/vivianechan/Documents/1.0\ Projects/2.0\ HandMeDown/HandMeDown/Pods/Target\ Support\ Files/Realm/Realm-dummy.m \
-  /Users/vivianechan/Documents/1.0\ Projects/2.0\ HandMeDown/HandMeDown/Pods/Target\ Support\ Files/Realm/Realm-prefix.pch
